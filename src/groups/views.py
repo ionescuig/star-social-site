1feb0d5324c1b9b652d6ac9d36bd0b5109134342
ionescuig <ionescuig@yahoo.com>
2018-10-26 06:26:10 +0100

    Added: groups: views,urls, templates,  posts: views,urls

--- src/groups/views.py
+++ src/groups/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Group, GroupMember
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
